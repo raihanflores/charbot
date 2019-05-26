740801e5e6d9316504839b5bd0dff6a20066402b
raihan <raihan.flores@gmail.com>
2019-05-27 01:59:31 +0800

    update

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.get('/webhook/', function(req, res) {
 
 app.post('/webhook/', function(req, res) {
     let messaging_events = req.body.entry[0].messaging
+    console.log(req.body.entry[0])
     let highestbid = 0
     for(let i = 0; i < messaging_events.length; i++) {
         let event = messaging_events[i]
