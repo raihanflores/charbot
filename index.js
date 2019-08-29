e1688feee973e9d72144cfce4f109674b4ab15e7
raihan <raihan.flores@gmail.com>
2019-08-30 03:11:22 +0800

    updates

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.post('/webhook/', (req, res) => {
 function sendText(sender, text) {
     let messageData = { text: text }
     request({
-        url: "https://graph.facebook.com/v4.0/me/messages",
+        url: "https://graph.facebook.com/v4.0/ryannsolaresflores/messages",
         qs: { access_token: VERIFY_TOKEN },
         method: 'POST',
         json: {
