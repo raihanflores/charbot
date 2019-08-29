e8c5fee66ba866cf9657700057cc54178c30be04
raihan <raihan.flores@gmail.com>
2019-08-30 01:09:32 +0800

    update

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ app.get('/webhook', (req, res) => {
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
-
+    console.log("mode", mode);
+    console.log("token", token);
+    console.log("challenge", challenge);
     // Checks if a token and mode is in the query string of the request
     if (mode && token) {
 
