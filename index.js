7f89e56256af9bc2e5e39c0f051cd9a8d0e1dbe7
raihan <raihan.flores@gmail.com>
2019-08-30 01:13:37 +0800

    update

--- index.js
+++ index.js
@@ -75,6 +75,58 @@ app.get('/webhook', (req, res) => {
     }
 });
 
+app.post('/webhook', (req, res) => {
+
+    // Token
+    let VERIFY_TOKEN = "EAAevWHxDRssBAJuxiZBQ2SXlCkcM1gm4JIfiPJyxjHbG5jPwmps2DpglusJznfz05pYQP6IK4B5Nls295uMKQnbYMZBs5CN9YrpEbcTU9CIRirAy0oEtVdGIefZA9sod0lWqbZB9o7qAWpgGCn87Fr3YTmhUJrV0s9LmY08nOwZDZD"
+
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+    console.log("mode", mode);
+    console.log("token", token);
+    console.log("challenge", challenge);
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+
+        // Checks the mode and token sent is correct
+        if (token === VERIFY_TOKEN) {
+
+            if(mode === "subscribe" ) {
+                res.status(200).send(challenge);
+            }
+            
+            let body = req.body;
+
+            // Iterates over each entry - there may be multiple if batched
+            body.entry.forEach(function (entry) {
+
+                // Gets the message. entry.messaging is an array, but 
+                // will only ever contain one message, so we get index 0
+                let messaging_events = entry.messaging[0];
+                for (let i = 0; i < messaging_events.length; i++) {
+                    let event = messaging_events[i]
+                    let sender = event.sender.id
+                    if (event.message && event.message.text) {
+                        let message = event.message.text;
+    
+                        if (message.includes("pricelist") || message.includes("pricelist")) {
+                            sendText(sender, getPriceList())
+                        }
+                    }
+                }
+            });
+
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
+});
+
 function sendText(sender, text) {
     let messageData = { text: text }
     request({
